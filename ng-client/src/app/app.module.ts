ceedbe1666f9adad66773f3418b0c33769fb62a7
Shehan <ggsmnandathilaka@gmail.com>
2021-12-05 10:28:12 +0530

    Front-end project structure setup and component creation.

--- ng-client/src/app/app.module.ts
+++ ng-client/src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {RouterModule} from "@angular/router";
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule,
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ ng-client/src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LoginComponent} from './login/login.component';
+import {SignUpComponent} from './sign-up/sign-up.component';
+import {HeaderComponent} from './header/header.component';
+import {FormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    SignUpComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule
+  ]
+})
+export class SharedModule {
+}
