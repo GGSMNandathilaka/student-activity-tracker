bcea56c6c46f5e48e369033d6a00829b15eea830
Shehan <ggsmnandathilaka@gmail.com>
2021-12-05 14:58:46 +0530

    Unsubscribe subscriptions of login & signup.

--- ng-client/src/app/shared/login/login.component.ts
+++ ng-client/src/app/shared/login/login.component.ts
@@ -1,17 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {User} from "../../core/model/user";
 import {AuthService} from "../../core/service/auth.service";
 import {Router} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   loggedInUserData: User = new User('', '', '', '');
   isLoginError = false;
+  loginSubscription: Subscription = new Subscription();
 
   constructor(private _auth: AuthService,
               private _router: Router) {
@@ -20,12 +22,18 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ngOnDestroy() {
+    if (this.loginSubscription) {
+      this.loginSubscription.unsubscribe();
+    }
+  }
+
   /**
    * Triggers when user click sign-in button
    */
   onUserLoggedIn() {
     this.isLoginError = false;
-    this._auth.loginUser(this.loggedInUserData).subscribe((res: any) => {
+    this.loginSubscription = this._auth.loginUser(this.loggedInUserData).subscribe((res: any) => {
         if (res) {
           // once login navigate to home page
           this._router.navigate(['']);
--- ng-client/src/app/shared/sign-up/sign-up.component.ts
+++ ng-client/src/app/shared/sign-up/sign-up.component.ts
@@ -1,16 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {User} from "../../core/model/user";
 import {AuthService} from "../../core/service/auth.service";
 import {Router} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent implements OnInit, OnDestroy {
 
   registeredUserData: User = new User('', '', '', '');
+  registerSubscription: Subscription = new Subscription();
 
   constructor(private _auth: AuthService,
               private _router: Router) {
@@ -19,8 +21,14 @@ export class SignUpComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  ngOnDestroy() {
+    if (this.registerSubscription) {
+      this.registerSubscription.unsubscribe();
+    }
+  }
+
   onRegisterUser() {
-    this._auth.registerUser(this.registeredUserData).subscribe((res: any) => {
+    this.registerSubscription = this._auth.registerUser(this.registeredUserData).subscribe((res: any) => {
         if (res) {
           // once login navigate to home page
           this._router.navigate(['']);
