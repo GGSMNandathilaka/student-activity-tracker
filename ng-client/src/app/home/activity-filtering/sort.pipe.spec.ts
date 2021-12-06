0dff9f338bc6a637e2e7fac84515aaf865818bd9
Shehan <ggsmnandathilaka@gmail.com>
2021-12-06 06:30:26 +0530

    Angular unit testing.

--- ng-client/src/app/home/activity-filtering/sort.pipe.spec.ts
+++ ng-client/src/app/home/activity-filtering/sort.pipe.spec.ts
@@ -1,8 +1,16 @@
-import { SortPipe } from './sort.pipe';
+import {SortPipe} from './sort.pipe';
 
 describe('SortPipe', () => {
   it('create an instance', () => {
     const pipe = new SortPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('sort a name (first-name last-name) array in alphabetical order by last-name', () => {
+    const pipe = new SortPipe();
+    const originalNameArr = ["Student 0", "Student 1", "Student 2", "Student 10"];
+    const sortedNameArr = ["Student 0", "Student 1", "Student 10", "Student 2"];
+
+    expect(pipe.transform(originalNameArr)).toBe(sortedNameArr);
   });
 });
